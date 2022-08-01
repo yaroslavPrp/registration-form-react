073a70941b29108e0f22d9d163f3e24ffc027176
Yaroslav Perepylytsia <perepylytsya95@gmail.com>
2022-08-01 13:42:50 +0300

    prettier enabled

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import React, {useState } from 'react';
-import Form from './containers/Form/Form';
-import Profile from './containers/Profile/Profile';
+import React, { useState } from "react";
+import Form from "./containers/Form/Form";
+import Profile from "./containers/Profile/Profile";
 
 const App = () => {
-	const [formStatus, setFormStatus] = useState({
-		formReady: false,
-		formData: {}
-	})
+  const [formStatus, setFormStatus] = useState({
+    formReady: false,
+    formData: {},
+  });
 
-	const toggleFormReadiness = (data) => {
-		setFormStatus({
-			formReady: true,
-			formData: data
-		})
-	}
+  const toggleFormReadiness = (data) => {
+    setFormStatus({
+      formReady: true,
+      formData: data,
+    });
+  };
 
-	return (
-		<>
-			{formStatus.formReady ?
-				<Profile data={formStatus.formData} /> :
-				<Form readyForm={toggleFormReadiness} />}
-		</>
-	);
+  return (
+    <>
+      {formStatus.formReady ? (
+        <Profile data={formStatus.formData} />
+      ) : (
+        <Form readyForm={toggleFormReadiness} />
+      )}
+    </>
+  );
 };
 
 export default App;
