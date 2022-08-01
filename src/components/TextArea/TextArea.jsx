9353b45bc51df38420d2d2fb68e4c18aa3813f84
Yaroslav Perepylytsia <perepylytsya95@gmail.com>
2022-08-01 13:39:44 +0300

    prettier enabled

--- src/components/TextArea/TextArea.jsx
+++ src/components/TextArea/TextArea.jsx
@@ -1,34 +1,40 @@
-import React, { Component } from 'react';
-import style from '../../containers/Form/form.module.css';
+import React, { Component } from "react";
+import style from "../../containers/Form/form.module.css";
 
 class TextArea extends Component {
-	onChange = (event) => {
-		this.props.handleChange(event);
-	}
+  onChange = (event) => {
+    this.props.handleChange(event);
+  };
 
-	render() {
-		const { rows, text, name, value, error, empty } = this.props;
+  render() {
+    const { rows, text, name, value, error, empty } = this.props;
 
-		return (
-			<label className={style.element}>{text}
-				{empty &&
-					<div className={style.emptyError}>*Поле пустое. Заполните, пожалуйста</div>
-				}
-				<textarea
-					value={value}
-					onChange={event => this.onChange(event)}
-					className={style.input}
-					rows={rows}
-					placeholder={text}
-					name={name}>
-				</textarea>
-				{error ?
-					<div className={style.error}>{error}</div> :
-					<div className={style.textAreaCounter}>{value.length}/{600}</div>
-				}
-			</label>
-		);
-	}
+    return (
+      <label className={style.element}>
+        {text}
+        {empty && (
+          <div className={style.emptyError}>
+            *Поле пустое. Заполните, пожалуйста
+          </div>
+        )}
+        <textarea
+          value={value}
+          onChange={(event) => this.onChange(event)}
+          className={style.input}
+          rows={rows}
+          placeholder={text}
+          name={name}
+        ></textarea>
+        {error ? (
+          <div className={style.error}>{error}</div>
+        ) : (
+          <div className={style.textAreaCounter}>
+            {value.length}/{600}
+          </div>
+        )}
+      </label>
+    );
+  }
 }
 
 export default TextArea;
--- src/utils/initialFormState.js
+++ src/utils/initialFormState.js
@@ -1,21 +1,21 @@
 export const formDefault = {
-	name: '',
-	surname: '',
-	birthDate: '',
-	phone: '',
-	website: '',
-	about: '',
-	technologies: '',
-	lastProject: ''
-}
+  name: "",
+  surname: "",
+  birthDate: "",
+  phone: "",
+  website: "",
+  about: "",
+  technologies: "",
+  lastProject: "",
+};
 
 export const formDefaultFalseProp = {
-	name: false,
-	surname: false,
-	birthDate: false,
-	phone: false,
-	website: false,
-	about: false,
-	technologies: false,
-	lastProject: false
-}
+  name: false,
+  surname: false,
+  birthDate: false,
+  phone: false,
+  website: false,
+  about: false,
+  technologies: false,
+  lastProject: false,
+};
